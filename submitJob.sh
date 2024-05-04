0782ea54d1372065065893ac27bd868a87e89739
saman <saman.noorzadeh@surgar-surgery.com>
2024-05-04 22:37:41 +0200

    initialization

--- submitJob.sh
+++ submitJob.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=3 # number of gpus per task
 
 #SBATCH --mem-per-gpu=20000M # Memory allocated per gpu
-#SBATCH --time=0-03:00:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --time=0-24:00:00  ## time for analysis (day-hour:min:sec)
 
 ## Run the script
 srun python train.py --epochs 300 --batch 10 --scheduler
