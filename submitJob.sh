a215c952c9b1963ce7b2437e2ab3bf7028290a38
saman <saman.noorzadeh@surgar-surgery.com>
2024-05-04 22:53:55 +0200

    initialization

--- submitJob.sh
+++ submitJob.sh
@@ -6,10 +6,10 @@
 #SBATCH --output=stdout
 #SBATCH --error=sdterr
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
-#SBATCH --gpus-per-task=1 # number of gpus per task
+#SBATCH --gpus-per-task=2 # number of gpus per task
 #SBATCH --cpus-per-task=3 # number of gpus per task
 
-#SBATCH --mem-per-gpu=20000M # Memory allocated per gpu
+#SBATCH --mem-per-gpu=24000M # Memory allocated per gpu
 #SBATCH --time=0-24:00:00  ## time for analysis (day-hour:min:sec)
 
 ## Run the script
